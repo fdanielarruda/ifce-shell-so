2f42f37e2956ec038cde79a65c27c5a9d1ad46c5
fdanielarruda <arruda.daniel20@gmail.com>
2023-02-28 19:29:34 -0300

    comentarios

--- myshell.c
+++ myshell.c
@@ -33,27 +33,29 @@ void read_command(char* params[]) {
 }
 
 // EXECUÇÃO DO PROGRAMA
-int main() {
+int main() {  
+    // LOOP INFINITO
     while(TRUE) {
-        type_prompt();
-        read_command(params);
+        type_prompt(); // EXIBE O PROMPT
+        read_command(params); // ENTRADA DO TECLADO
 
         // exit FINALIZA O SHELL
         if (strcmp(params[0], "exit") == 0) {
             break;
         }
 
-        pid_t pid = fork();
+        pid_t pid = fork(); // CRIA UM PROCESSO FILHO
 
+        // VALIDAÇÃO DE POSSÍVEL ERRO NA EXECUÇÃO
         if (pid < 0) {
-            printf("Erro ao realizar comando.\n");
+            printf("Erro ao realizar o fork.\n");
             continue;
         }
 
         if (pid != 0) {
-            waitpid(-1, &status, 0);
+            waitpid(-1, &status, 0); // PAI ESPERA O FILHO
         } else {
-            execvp(params[0], params);
+            execvp(params[0], params); // FILHO EXECUTA O COMANDO
             fprintf(stderr, "Erro ao executar o comando.\n");
             exit(EXIT_FAILURE);
         }
